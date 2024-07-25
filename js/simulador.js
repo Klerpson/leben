67437c30f290d30599552a1b4fd8aa864903fcb2
Julian DW <klerpson@gmail.com>
2024-07-25 09:30:32 -0500

    gast

--- js/simulador.js
+++ js/simulador.js
@@ -6,16 +6,19 @@ function calculateExpenses() {
       return;
   }
 
-  const notaryFeeRate = 0.002; 
-  const registryFeeRate = 0.01;
-  const certificatesFee = 50000;
+  const withholdingTaxRate = 0.01; // Retención en la fuente
+  const registryBeneficenceRate = 0.0167; // Beneficencia y registro
+  const notaryFeeRate = 0.003; // Gastos notariales
+  const certificatesFee = 20000; // Certificado de libertad
+  const iva = 114000; // IVA
 
+  const withholdingTax = propertyValue * withholdingTaxRate;
+  const registryBeneficenceFees = propertyValue * registryBeneficenceRate;
   const notaryFees = propertyValue * notaryFeeRate;
-  const registryFees = propertyValue * registryFeeRate;
-  const totalFees = notaryFees + registryFees + certificatesFee;
+  const totalFees = withholdingTax + registryBeneficenceFees + notaryFees + certificatesFee + iva;
 
   document.getElementById('notaryFees').innerText = notaryFees.toLocaleString('es-CO');
-  document.getElementById('registryFees').innerText = registryFees.toLocaleString('es-CO');
+  document.getElementById('registryFees').innerText = registryBeneficenceFees.toLocaleString('es-CO');
   document.getElementById('certificatesFees').innerText = certificatesFee.toLocaleString('es-CO');
   document.getElementById('totalFees').innerText = totalFees.toLocaleString('es-CO');
 
